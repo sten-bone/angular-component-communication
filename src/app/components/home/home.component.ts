0cb62e0b37e95c8499f9db25fee52cae5c51b5f9
Ben Stone <benstone@uiowa.edu>
2022-06-13 10:31:57 -0500

    Add home page information

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,18 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-      home works!
-    </p>
+    <div class="m-3">
+      <h1>Home</h1>
+      <p>
+        This site is used as a practice sandbox for communication techniques
+        between Angular components. The techniques are organized by category in
+        the navigation bar above.
+      </p>
+      <p>Written by Ben Stone 2022 using Angular and Bootstrap.</p>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
