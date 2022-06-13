bb71f30a2402f8ab7bf9fe641b5974347c97ecc1
Ben Stone <benstone@uiowa.edu>
2022-06-13 09:34:00 -0500

    Create book data and detail display

---
+++ src/app/components/ptc/book-data.ts
@@ -0,0 +1,44 @@
+import { Book } from '../../models/book';
+
+export const BOOKS: Book[] = [
+  {
+    isbn: '9781847490599',
+    title: 'Anna Karenina',
+    authorFirstName: 'Leo',
+    authorLastName: 'Tolstoy',
+    genre: 'Realist novel',
+    numPages: 864,
+  },
+  {
+    isbn: '9780446310789',
+    title: 'To Kill a Mockingbird',
+    authorFirstName: 'Harper',
+    authorLastName: 'Lee',
+    genre: 'Southern gothic',
+    numPages: 281,
+  },
+  {
+    isbn: '9780333791035',
+    title: 'The Great Gatsby',
+    authorFirstName: 'F. Scott',
+    authorLastName: 'Fitzgerald',
+    genre: 'Tragedy',
+    numPages: 208,
+  },
+  {
+    isbn: '9780060114183',
+    title: '100 Years of Solitude',
+    authorFirstName: 'Gabriel Garcia',
+    authorLastName: 'Marquez',
+    genre: 'Magic realism',
+    numPages: 417,
+  },
+  {
+    isbn: '9780060188702',
+    title: 'Don Quixote',
+    authorFirstName: 'Miguel',
+    authorLastName: 'de Cervantes',
+    genre: 'Novel',
+    numPages: 928,
+  },
+];
